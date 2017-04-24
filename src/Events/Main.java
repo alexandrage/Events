20f7145a5e99d9130bad8e4f80cd868964b305d7
alexandrage <alexandrage@mail.ru>
2017-04-24 10:36:39 +0300

    UP

--- src/Events/Main.java
+++ src/Events/Main.java
@@ -10,8 +10,8 @@ public class Main {
 		new Plugin2();
 		MyEvent e = new MyEvent("test");
 		MyEvent2 e2 = new MyEvent2(Arrays.asList(new String[]{"list1","list2"}));
-		MyEvent ev1 = RegisterEvent.callEvent(e);
-		MyEvent2 ev2 = RegisterEvent.callEvent(e2);
+		MyEvent ev1 = HandlerList.callEvent(e);
+		MyEvent2 ev2 = HandlerList.callEvent(e2);
 		if(ev1.isCancelled()) {
 			System.out.println("MyEvent isCancelled");
 		}
